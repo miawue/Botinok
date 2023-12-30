c905ae4a0437e3dcf850ea7c73633d891374f62d
Morozov Kirill <reddocwork@gmail.com>
2023-12-30 09:33:23 +0300

    added README.md

---
+++ commands/play.ts
@@ -0,0 +1,143 @@
+const declOfNum = require("../utils/declOfNum");
+
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { EmbedBuilder } = require("discord.js")
+const { QueryType } = require("discord-player")
+const {
+    YouTubeExtractor
+ } = require('@discord-player/extractor');
+
+const playFunc = async (player, interaction) => {
+    player.extractors.register(YouTubeExtractor);
+    const channel = interaction.member.voice.channel;
+    if (!channel) return interaction.reply('Необходимо находится в голосовом канале'); // Убедимся что пользователь в голосовом канале
+    const query = interaction.options.getString('url', true);
+
+    try {
+        const { track } = await player.play(channel, query, {
+            nodeOptions: {
+                // nodeOptions are the options for guild node (aka your queue in simple word)
+                metadata: interaction // we can access this metadata object using queue.metadata later on
+            }
+        });
+
+        return await interaction.followUp(`**${track.title}** воспроизводится!`);
+    } catch (e) {
+        // Если вдруг что-то пошло не так
+        return await interaction.followUp(`Что-то пошло не так: ${e}`);
+    }
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("play")
+		.setDescription("Начинает проигрывание трека с YouTube")
+		// .addSubcommand(subcommand =>
+		// 	subcommand
+		// 		.setName("search")
+		// 		.setDescription("Searches for a song and plays it")
+		// 		.addStringOption(option =>
+		// 			option.setName("searchterms").setDescription("search keywords").setRequired(true)
+		// 		)
+		// )
+        // .addSubcommand(subcommand =>
+		// 	subcommand
+		// 		.setName("playlist")
+		// 		.setDescription("Начинает проигрывание плейлиста с YouTube")
+		// 		.addStringOption(option => option.setName("url").setDescription("Ссылка на плейлист").setRequired(true))
+		// )
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("song")
+				.setDescription("Начинает проигрывание одного трека с YouTube.")
+				.addStringOption(option => option.setName("url").setDescription("Ссылка на трек").setRequired(true))
+		),
+	execute: async ({ client, interaction }) => {
+        // Проверка на то что пользователь в войс канале
+		if (!interaction.member.voice.channel) return interaction.reply("Для вызова команды необходимо находится в голосовом канале.");
+
+        // Создает очередь проигрывания для сервера
+		const queue = await client.player.nodes.create(interaction.guild);
+
+        // Ждет пока пользователь подключится к голосовому чату
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
+
+		let embed = new EmbedBuilder()
+
+		if (interaction.options.getSubcommand() === "song") {
+            let url = interaction.options.getString("url")
+            
+            
+            // Ищет трек с помощью DiscordPlayer
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_VIDEO
+            })
+
+            // Падает в ошибку если ничего не нашел
+            if (result.tracks.length === 0)
+                return interaction.reply("Ничего не было найдено")
+
+            // Добавляет трек в очередь
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** был добавлена в очередь!`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Длительность: ${song.duration}`})
+
+		}
+
+        else if (interaction.options.getSubcommand() === "playlist") {
+
+            // Ищем плейлист с помощью DiscordPlayer
+            let url = interaction.options.getString("url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_PLAYLIST
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.reply(`По адресу ${url} не было найдено плейлиста`)
+            
+            // Добавляет треки в очередь
+            const playlist = result.playlist
+            await queue.addTracks(result.tracks)
+            embed
+                .setDescription(`**${result.tracks.length} ${declOfNum(result.tracks.length, ['трек', 'трека', 'треков'])} [${playlist.title}](${playlist.url})** было добавлено в очередь`)
+                .setThumbnail(playlist.thumbnail)
+
+		} 
+        else if (interaction.options.getSubcommand() === "search") {
+
+            // Ищем трек с помощью DiscordPlayer
+            let url = interaction.options.getString("searchterms")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO
+            })
+
+            // Выкидываем ошибку если не было найдень
+            if (result.tracks.length === 0)
+                return interaction.editReply("Ничего не было найдено")
+            
+            // Добавляем трек в очередь
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** был добавлен в очередь`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Длительность: ${song.duration}`})
+		}
+
+        // Играем трек
+        if (!queue.playing) {
+            playFunc(client.player, interaction)
+
+            // Отвечаем с информацией о проигрывателе
+            await interaction.reply({
+                embeds: [embed]
+            })
+        }
+	},
+}
---
+++ index.js
@@ -0,0 +1,94 @@
+require('dotenv').config()
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Player } = require('discord-player');
+
+const { readdirSync } = require('node:fs');
+const { join } = require('node:path');
+const { error } = require('node:console');
+const { _ } = require('lodash');
+const declOfNum = require('./utils/declOfNum');
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+})
+
+// Загружает все команды
+
+const commands = [];
+client.commands = new Collection()
+
+const commandsPath = join(__dirname, 'commands');
+const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+    const filePath = join(commandsPath, file)
+    const command = require(filePath)
+
+    if (_.isEmpty(command)) continue;
+
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON())
+}
+
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: 'highestaudio',
+        highWaterMark: 1 << 25
+    }
+})
+client.player.extractors.loadDefault();
+
+// Добавляем слэш-команды на сервер
+
+client.on('ready', async () => {
+    const guildIds = client.guilds.cache.map(guild => guild.id)
+    const guildName = client.guilds.cache.map(guild => guild.name)
+
+
+    const rest = new REST({version: '9'}).setToken(process.env.TOKEN);
+
+    for (const guildId of guildIds) {
+
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId), {
+            body: commands
+        })
+        .then((smth) => {
+            console.log(`Было добавлено ${commands.length} ${declOfNum(commands.length, ['команда', 'команды', 'команд'])} для ${guildName}`)
+        })
+        .catch((error) => console.error(`Ошибка добавления команд для ${guildId} \n ${error}`))
+    }
+
+})
+
+// Вызываем команды
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute({client, interaction})
+    } catch (error) {
+        console.log(command)
+        console.error(`Ошибка вызова команды ${command.data.name} \n ${error}`)
+        await interaction.reply('Произошла ошибка вызова команды. Напишите <@294244367655501824> что ему пора на завод а не заниматься этой хуйней')
+    }
+
+})
+
+// Отлавливаем ошибку
+client.on('error', error => {
+    console.error('The WebSocket encountered an error:', error);
+});
+
+client.login(process.env.TOKEN)
