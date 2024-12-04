e3425ebc7bb80708bda065bb995da7e7adf5096c
Morozov Kirill <tornado40lvl2@mail.ru>
2024-12-04 18:02:04 +0300

    приколы

--- events/player/addList.js
+++
@@ -1,5 +0,0 @@
-const db = require("../../mongoDB");
-module.exports = async (client, queue, playlist) => {
-
-queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length + " " + '❌'})\` ❌❌` }).catch(e => { })
-}
--- events/player/finish.js
+++
@@ -1,5 +0,0 @@
-const db = require("../../mongoDB");
-module.exports = async (client, queue) => {
-
-
-}
